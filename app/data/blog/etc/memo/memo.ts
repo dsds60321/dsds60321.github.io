2f96bf6728337cdf60856a7ab483f41a3826bb6a
dsds60321 <dsds60321@gmail.com>
2025-04-21 23:46:00 +0900

    아스키독스

--- app/data/blog/etc/memo/memo.ts
+++ app/data/blog/etc/memo/memo.ts
@@ -2,7 +2,8 @@ import { BlogPost } from '@/app/types/blog';
 import { post1 } from './contents/post1';
 import { post2 } from './contents/post2';
 import { post3 } from './contents/post3';
+import { post4 } from './contents/post4';
 
 export const memoPosts: BlogPost[] = [
-   post1,post2,post3
+   post1,post2,post3,post4
 ];
