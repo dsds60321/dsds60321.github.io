a10bbf8b313327925202a33f0d031f8a686974b9
kanggeonho <kanggeonho@kanggeonhoui-MacBookPro.local>
2025-11-19 22:05:29 +0900

    config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
+    distDir: 'out',
     images: {
         unoptimized: true,
     },
+    trailingSlash: true,
+    basePath: '',
+    assetPrefix: '',
 };
 
 export default nextConfig;
